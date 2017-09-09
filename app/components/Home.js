a32c969abb23dde5e96bf2bbe7242fc51d7a04ca
JayaArasalike <mail@jayalaxmi.info>
2017-09-08 23:39:10 -0700

    results state in home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,6 +6,17 @@ import API from "../utils/API";
 //<div className="jumbotron" style={{backgroundColor: '#20315A', color: 'white'}}>
 
 class Home extends Component {
+	state = {
+		articles: []
+	};
+	// Getting all quotes when the component mounts
+
+	getArticles = (term, startDate, endDate) => {
+		API.getArticles(term, startDate, endDate).then(function(results){
+			console.log("results", results);
+		})
+	}
+	
 	render() {
 		return(
 			<div className="container">
@@ -13,7 +24,7 @@ class Home extends Component {
 			    <div className="jumbotron">
 			      <h1 className="text-center"><strong><i className="fa fa-newspaper-o"></i> New York Times Search</strong></h1>
     			</div>
-    			<Search />
+    			<Search getArticles={this.getArticles} />
     			<Results />
     		</div>
 
